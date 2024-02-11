fb3801a859db3754c7c73c919a86c9dd3b218037
Sophia Silovsky <ssilovsky@smith.edu>
2024-02-11 13:56:02 -0500

    added num beginnings

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner sc = new Scanner(System.in);
+
+    int input_num = 0;
+    int round_num = 0;
+
+    System.out.println("How many rounds?");
+    input_num = sc.nextInt();
+    sc.close();
+
+    while (round_num != input_num) {
+
+      System.out.println("yippee!");
+      round_num += 1;
+
+
+    }
   }
 }
